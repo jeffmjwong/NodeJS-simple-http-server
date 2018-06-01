ef6c823fc90b4e2af07709a77d6313ef76a3fd8b
Jeff Wong <jeffmjwong@gmail.com>
2018-06-01 12:08:50 +1000

    Add GET and POST request and response

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
 
-  res.write('Hello from our server!');
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  if (req.url === '/hello') {
+    res.write('<h2>Hello from our server!</h2>');
+  } else if (req.url === '/foo') {
+    if (req.method === 'GET') {
+      res.write('<h2>FOO WAS HERE!!! BOOYAH!</h2>');
+    } else if (req.method === 'POST') {
+      res.write('<h2>YOU POSTED FOO!</h2>');
+    }
+  }
   res.write('<h1>Goodbye!</h1>');
 
   res.end();
 });
 
 server.listen(4321);
+console.log('Server listening at localhost:4321');
